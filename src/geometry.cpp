cdb760f8e0a2080b47c212419cf156a0f6d1404a
Aristide <aristide.cuny@gmail.com>
2022-04-08 08:28:54 +0000

    new method with culling make -j12

--- src/geometry.cpp
+++ src/geometry.cpp
@@ -11,57 +11,37 @@
 //  Right now : doesn't compute the exact point, works only for triangles, and isn't optimal anyway
 aiVector3t<double> planeIntersect(const aiFace &face, const aiMesh &mesh, const aiVector3t<double> &origin, const aiVector3t<double> &ray,
                                  const aiVector3t<double> &nullvalue) {
-    if (face.mNumIndices > 3)
-    {
-        std::cout << "I'm not a triangle !\n";
-        return nullvalue;
-    }
 
     double kEpsilon = 0.0000001f;
     aiVector3t<double> v0 = mesh.mVertices[face.mIndices[0]];
     aiVector3t<double> v1 = mesh.mVertices[face.mIndices[1]];
     aiVector3t<double> v2 = mesh.mVertices[face.mIndices[2]];
 
-    aiVector3t<double> a = v1 - v0;
-    aiVector3t<double> b = v2 - v1;
-    aiVector3t<double> c = v0 - v2;
+    const auto edge1 = v1 - v0;
+    const auto edge2 = v2 - v0;
 
-    // compute the face's normal vector
-    aiVector3t<double> n = a ^ b;
+    const auto pvec = ray ^ edge2;
+    const auto det = edge1 * pvec;
 
-    // if the normal and the ray direction have the same sign, then the triangle
-    // is facing the wrong way
-    if (ray * n >= 0)
+    if (det < kEpsilon)
         return nullvalue;
 
-    // compute the distance from the plane to (0, 0, 0)
-    // (aka the fourth plane equation component)
-    double D = -(n * v0);
-    double t
-            = -(n * origin + D) / (n * ray);
-    if (t < 0)
+    const auto tvec = origin - v0;
+
+    const auto u = tvec * pvec;
+    if (u < 0 || u > det)
+        return nullvalue;
+    
+
+    const auto qvec = tvec ^ edge1;
+
+    const auto v = ray * qvec;
+    if (v < 0 || u + v > det)
         return nullvalue;
 
-    // P = O + t * dir
-    aiVector3t<double> P_off = ray * t;
-    aiVector3t<double> P = origin + P_off;
+    const auto t = edge2 * qvec;
 
-    // check on which side of a, b, and c P is
+    const auto inv_det = 1 / det;
 
-    aiVector3t<double> v0_to_p = P - v0;
-    aiVector3t<double> v0_cross = a ^ v0_to_p;
-    if (v0_cross * n < -kEpsilon)
-        return nullvalue;
-
-    aiVector3t<double> v1_to_p = P - v1;
-    aiVector3t<double> v1_cross = b ^ v1_to_p;
-    if (v1_cross * n < -kEpsilon)
-        return nullvalue;
-
-    aiVector3t<double> v2_to_p = P - v2;
-    aiVector3t<double> v2_cross = c ^ v2_to_p;
-    if (v2_cross * n < -kEpsilon)
-        return nullvalue;
-
-    return P;
+    return aiVector3t<double>(t * inv_det, u * inv_det, v * inv_det);
 }
