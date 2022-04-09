53d41d57a1fc18bf06d1bb57a9784bc2d1cb480b
Aristide <aristide.cuny@gmail.com>
2022-04-09 20:29:34 +0000

    added DSM object

---
+++ src/DSM.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <vector>
+#include <algorithm>
+#include "Camera.h"
+#include "PointLight.h"
+
+#define DSM_SAMPLES 20000
+
+class DSM
+{
+
+
+
+public:
+    class Visibility
+    {
+        private:
+            std::vector<double> sample_zs;
+            std::vector<double> sample_Vs;
+        public:
+            Visibility(const std::vector<double> &sample_zs, const std::vector<double> &sample_Vs)
+            : sample_zs(sample_zs), sample_Vs(sample_Vs)
+            {}
+            void compress();
+            double function(const double z) const ;
+    };
+
+    const unsigned int height;
+    const unsigned int width;
+    const unsigned int size;
+    DSM(unsigned int height, unsigned int width)
+    : height(height), width(width), size(height * width)
+    {
+        visibilities = std::vector<Visibility>();
+    }
+    static Camera defaultCameraFromPointLight(PointLight pointLight);
+    void draw(Camera camera);
+    Visibility visibilityAt(unsigned int h, unsigned int w) const;
+    private:
+
+        std::vector<Visibility> visibilities;
+};
